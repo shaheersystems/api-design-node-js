97b1b61a3d22b63455ebd30e20854e901110bf8e
shaheersystems <shaheer.imran1429@gmail.com>
2023-09-24 01:49:46 +0500

    minor fix

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,3 @@ import app from "./server";
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
 });
-
-("postgres://shaheersystems:@ep-silent-sunset-53045846.us-west-2.aws.neon.tech/neondb");
